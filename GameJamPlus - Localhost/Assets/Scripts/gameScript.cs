ee94d94f3eb6731f1c6064af47b1cb95aa25d340
BeavisBuss <beavis.buss@gmail.com>
2020-10-10 17:35:10 -0300

    gameScript

---
+++ GameJamPlus - Localhost/Assets/Scripts/gameScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameScript : MonoBehaviour
+{
+    public int nPlayers;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
