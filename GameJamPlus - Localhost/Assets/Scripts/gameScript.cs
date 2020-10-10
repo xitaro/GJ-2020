2aef0da0fd79b76f6c6b0511a62c579bfc473538
GuterresTiago <tiagoguterresdossantos@hotmail.com>
2020-10-10 18:44:34 -0300

    Seleciona

--- GameJamPlus - Localhost/Assets/Scripts/gameScript.cs
+++ GameJamPlus - Localhost/Assets/Scripts/gameScript.cs
@@ -4,18 +4,34 @@ using UnityEngine;
 
 public class gameScript : MonoBehaviour
 {
-    public int nPlayers;
-   
-
+    float time;
+    int i,a;
+    bool b=true;
+    public GameObject spaw;
+    public GameObject[] players;
+    [SerializeField] float Timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime;
+        if (time <= Timer-1)
+        {
+            players = GameObject.FindGameObjectsWithTag("Player");
+            i = players.Length;
+            a = Random.Range(0, i);
+            spaw = players[a];
+        }
+        if (time>=Timer && b == true)
+        {
+            spaw.transform.gameObject.tag = "Enemy";
+            b = false;
+        }
     }
+ 
 }
