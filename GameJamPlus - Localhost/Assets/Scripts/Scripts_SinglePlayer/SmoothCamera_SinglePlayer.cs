4972edd2fb88de75f412fbdde2443e837ca8d281
xitaro <marcobsaraiva@gmail.com>
2020-10-10 16:51:07 -0300

    Camera follow ok

--- GameJamPlus - Localhost/Assets/Scripts/Scripts_SinglePlayer/SmoothCamera_SinglePlayer.cs
+++ GameJamPlus - Localhost/Assets/Scripts/Scripts_SinglePlayer/SmoothCamera_SinglePlayer.cs
@@ -8,19 +8,19 @@ public class SmoothCamera_SinglePlayer : MonoBehaviour//NetworkBehaviour
 {
     [Header("Reference")]
     // Player or any other object to track with camera
-    [SerializeField] private Transform lookAt = null;
-    [SerializeField] private CinemachineVirtualCamera virtualCamera = null;
+    //[SerializeField] private Transform lookAt = null;
+   // [SerializeField] private CinemachineVirtualCamera virtualCamera = null;
 
     private CinemachineTransposer transposer;
 
     private void Start()
     {
-        transposer = virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+        //transposer = virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
 
         //enable camera
-        virtualCamera.gameObject.SetActive(true);
+        //virtualCamera.gameObject.SetActive(true);
 
-        enabled = true;
+        //enabled = true;
     }
 
     /*
