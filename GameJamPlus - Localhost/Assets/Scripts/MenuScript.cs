d8484293a24a6da47bd1b5caef84a0efad266659
FehMello <72583255+FehMello@users.noreply.github.com>
2020-10-10 21:19:48 -0300

    Animacoes no menu e scripts
    animacoes e scripts funcionando, precisa otimizar

---
+++ GameJamPlus - Localhost/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public Animator menuAnimator;
+    public GameObject Title;
+    public GameObject BtnPlay;
+    public GameObject BtnOpcoes;
+    public GameObject BtnAbout;
+    public GameObject BtnReturn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menuAnimator = gameObject.GetComponent<Animator>();
+        BtnReturn.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {    
+        SceneManager.LoadScene(1);
+    }
+
+    public void Settings()
+    {
+            
+        menuAnimator.SetBool("IsSettings", true);
+        BtnOpcoes.SetActive(false);
+        Title.SetActive(false);
+        BtnPlay.SetActive(false);
+        BtnAbout.SetActive(false);
+        BtnReturn.SetActive(true);
+
+    }
+
+    public void Return()
+    {
+        if (menuAnimator.GetBool("IsSettings") == true)
+        {
+            menuAnimator.SetBool("IsSettings", false);          
+            BtnReturn.SetActive(false);
+            BtnOpcoes.SetActive(true);
+            Title.SetActive(true);
+            BtnPlay.SetActive(true);
+            BtnAbout.SetActive(true);
+        }
+        else
+        {
+            menuAnimator.SetBool("IsAbout", false);         
+            BtnReturn.SetActive(false);
+            BtnAbout.SetActive(true);
+            Title.SetActive(true);
+            BtnPlay.SetActive(true);
+            BtnAbout.SetActive(true);
+            BtnOpcoes.SetActive(true);
+        }
+        
+
+    }
+
+    public void About()
+    {       
+        menuAnimator.SetBool("IsAbout", true);    
+        BtnOpcoes.SetActive(false);
+        Title.SetActive(false);
+        BtnPlay.SetActive(false);
+        BtnAbout.SetActive(false);
+        BtnReturn.SetActive(true);
+       
+    }
+
+  
+}
+  
+
+
